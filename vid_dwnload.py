1d2ece50805cbab8e01b9ad12158f3ee1b3268ee
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-09 00:33:45 +0100

    Update vid_dwnload.py

--- vid_dwnload.py
+++ vid_dwnload.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pytube import YouTube, request
+from pytube import YouTube
 
 def download_video(url, quality='highest'):
     try:
@@ -7,10 +7,7 @@ def download_video(url, quality='highest'):
         if not "youtube.com" in url and not "youtu.be" in url:
             raise ValueError("Invalid YouTube URL")
         
-        # Set custom request headers
-        request.default_headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        
-        yt = YouTube(url)
+        yt = YouTube(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
         
         # Select stream based on quality
         if quality == 'highest':
