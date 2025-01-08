ab457829d4a6306d8df087d8319394548d29a90c
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-09 00:36:30 +0100

    Update vid_dwnload.py

--- vid_dwnload.py
+++ vid_dwnload.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from pytube import YouTube
+import pytube.request
+
+# Set a custom User-Agent header
+pytube.request.default_headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
 def download_video(url, quality='highest'):
     try:
