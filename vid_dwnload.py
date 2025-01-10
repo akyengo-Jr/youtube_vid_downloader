f604157f58d46bd91647f6499977f160adbd3223
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-10 17:51:29 +0100

    Update vid_dwnload.py#

--- vid_dwnload.py
+++ vid_dwnload.py
@@ -5,7 +5,10 @@ def download_video(url):
     ydl_opts = {
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-        }
+        },
+        'extractor_retries': 3,
+        'format': 'best',
+        'noplaylist': True
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
