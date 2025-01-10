12dd973a941558edae23536986373ef8753524ae
Goodness O. Akyengo <154751971+akyengo-Jr@users.noreply.github.com>
2025-01-10 17:47:39 +0100

    Update vid_dwnload.py

--- vid_dwnload.py
+++ vid_dwnload.py
@@ -1,14 +1,13 @@
 import streamlit as st
-import yt_dlp
+import youtube_dl
 
 def download_video(url):
     ydl_opts = {
-        'proxy': 'http://38.45.44.106:999',  # Replace with your proxy server details
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
         }
     }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         title = info['title']
         st.write(f"Downloading: {title}")
